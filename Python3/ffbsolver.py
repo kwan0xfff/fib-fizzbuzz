a6711644f2f34b8a9d5570713870ef4c5c898b0f
R. J. Dev. Kwan <kwan0xfff@gmail.com>
2017-09-15 17:37:21 -0700

    Python code mods.
    * Move 'oddSeries' out of class Primes.
    * Use oddSeries as generator for candidates for primes.
    * Add '-v' for verbose option.
    * Introduce "verbose" optional parameter in function classify().

--- Python3/ffbsolver.py
+++ Python3/ffbsolver.py
@@ -14,18 +14,22 @@ import sys
 from math import sqrt
 import pdb
 
+def oddSeries():
+    "Generate all odd numbers, starting at 1."
+    odd = 1
+    while True:
+        yield odd
+        odd += 2
+
 class Primes:
     "Progressively grown list of prime numbers."
     def __init__(self):
         self.primes = [ 2, 3 ]
         self.lastodd = self.primes[-1]
-
-    def oddSeries(self):
-        "Generate all odd numbers, starting at 5."
-        odd = 5
-        while True:
-            yield odd
-            odd += 2
+        self.odds = oddSeries()
+        got = next(self.odds)
+        while self.lastodd > got:
+            got = next(self.odds)
 
     def isPrime(self, candidate):
         "Return True if candidate is prime, else False."
@@ -46,31 +50,36 @@ class Primes:
         if limit < self.lastodd:
             return  # too soon to stretch
         while True:
-            self.lastodd += 2
+            self.lastodd = next(self.odds)
+            # print ("self.lastodd", self.lastodd)  # very verbose
             if self.isPrime(self.lastodd):
                 self.primes.append(self.lastodd)
             if self.lastodd >= limit:
                 return
 
-def classify(value, primes):
-    #   "Buzz" when F(n) is divisible by 3.
-    #   "Fizz" when F(n) is divisible by 5.
-    #   "FizzBuzz" when F(n) is divisible by 15.
-    #   "BuzzFizz" when F(n) is prime.
-    #   the value F(n) otherwise.
+def classify(value, primes, verbose=False):
+    """Generate messages for matched criteria:
+    * "Buzz" when F(n) is divisible by 3.
+    * "Fizz" when F(n) is divisible by 5.
+    * "FizzBuzz" when F(n) is divisible by 15.
+    * "BuzzFizz" when F(n) is prime.
+    * the value F(n), if none of the above.
+    """
 
     primes.stretch(int(sqrt(value)))
 
-    # look for desired divisors,
+    vv = verbose
     msgs = []
+    # look for desired divisors
     if value % 5 == 0:  # divisible by 5
-        msgs.append("Fizz5")
+        msgs.append("Fizz" + ["","5"][vv])
+        #msgs.append("Fizz" + verbose?"":"5")
     if value % 3 == 0:  # divisible by 3
-        msgs.append("Buzz3")
+        msgs.append("Buzz" + ["","3"][vv])
     if value % 15 == 0: # divisible by 15
-        msgs.append("FizzBuzz15")
+        msgs.append("FizzBuzz" + ["","15"][vv])
     if primes.isPrime(value):
-        msgs.append ("BuzzFizzP")
+        msgs.append ("BuzzFizz" + ["","P"][vv])
 
     if msgs:
         msg = ' '.join(msgs)
@@ -104,7 +113,6 @@ def parseargs(argv):
     parser.add_argument('-v', '--verbose', help="enable verbose output",
         action="store_true")
 
-    pdb.set_trace()
     args = parser.parse_args()
     return args
 
@@ -119,7 +127,7 @@ def main(argv):
         # fn is F(n)
         if args.verbose:
             print(fn, end=' ')          # prepend result by number and blank.
-        print(classify(fn, primes))
+        print(classify(fn, primes, verbose=args.verbose))
 
 
 sys.exit(main(sys.argv))
